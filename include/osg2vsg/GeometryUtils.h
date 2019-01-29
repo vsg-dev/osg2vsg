396fadfd43124f9bc5722a60f936744f0241cd23
tomhog <thomas.hogarth@gmail.com>
2019-01-29 13:54:51 +0000

    Added attributre requirements to geometry convert func. generate defaults if none in geometry
    This should allow us to either copy everything, or tell the function to only copy the part the graphics pipline supports

--- include/osg2vsg/GeometryUtils.h
+++ include/osg2vsg/GeometryUtils.h
@@ -12,12 +12,14 @@ namespace osg2vsg
 {
     enum GeometryAttributes
     {
-        VERTEX,
-        NORMAL,
-        COLOR,
-        TEXCOORD0,
-        TEXCOORD1,
-        TEXCOORD2
+        VERTEX = 1,
+        NORMAL = 2,
+        COLOR = 4,
+        TEXCOORD0 = 8,
+        TEXCOORD1 = 16,
+        TEXCOORD2 = 32,
+        STANDARD_ATTS = VERTEX | NORMAL | COLOR | TEXCOORD0,
+        ALL_ATTS = VERTEX | NORMAL | COLOR | TEXCOORD0 | TEXCOORD1 | TEXCOORD2
     };
 
 
@@ -29,5 +31,7 @@ namespace osg2vsg
 
     extern OSG2VSG_DECLSPEC vsg::ref_ptr<vsg::Data> convertToVsg(osg::Array* inarray);
 
-    extern OSG2VSG_DECLSPEC vsg::ref_ptr<vsg::Geometry> convertToVsg(osg::Geometry* geometry);
+    extern OSG2VSG_DECLSPEC uint32_t calculateAttributesMask(osg::Geometry* geometry);
+
+    extern OSG2VSG_DECLSPEC vsg::ref_ptr<vsg::Geometry> convertToVsg(osg::Geometry* geometry, uint32_t requiredAttributesMask = 0);
 }
