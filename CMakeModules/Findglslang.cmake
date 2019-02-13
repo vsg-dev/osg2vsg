f819545775d9883c6f9d822a32f59d4aaa1a56d2
Robert Osfield <robert@openscenegraph.com>
2019-02-13 10:02:38 +0000

    Refactored how GLSLang libs are linked to enable GLSlang compile differences to be handled better.

--- CMakeModules/Findglslang.cmake
+++ CMakeModules/Findglslang.cmake
@@ -46,6 +46,14 @@ find_library(SPIRV_LIBRARY
     NAMES SPIRV
 )
 
+find_library(SPIRV-Tools_LIBRARY
+    NAMES SPIRV-Tools
+)
+
+find_library(SPIRV-Tools-opt_LIBRARY
+    NAMES SPIRV-Tools-opt
+)
+
 find_library(OGLCompiler_LIBRARY
     NAMES OGLCompiler
 )
@@ -56,25 +64,33 @@ find_library(HLSL_LIBRARY
 
 if(WIN32)
 
-  find_library(glslang_LIBRARY_debug
-      NAMES glslangd
-  )
+    find_library(glslang_LIBRARY_debug
+        NAMES glslangd
+    )
 
-  find_library(OSDependent_LIBRARY_debug
-      NAMES OSDependentd
-  )
+    find_library(OSDependent_LIBRARY_debug
+        NAMES OSDependentd
+    )
 
-  find_library(SPIRV_LIBRARY_debug
-      NAMES SPIRVd
-  )
+    find_library(SPIRV_LIBRARY_debug
+        NAMES SPIRVd
+    )
 
-  find_library(OGLCompiler_LIBRARY_debug
-      NAMES OGLCompilerd
-  )
+    find_library(SPIRV-Tools_LIBRARY_debug
+        NAMES SPIRV-Toolsd
+    )
 
-  find_library(HLSL_LIBRARY_debug
-      NAMES HLSLd
-  )
+    find_library(SPIRV-Tools-opt_LIBRARY_debug
+        NAMES SPIRV-Tools-optd
+    )
+
+    find_library(OGLCompiler_LIBRARY_debug
+        NAMES OGLCompilerd
+    )
+
+    find_library(HLSL_LIBRARY_debug
+        NAMES HLSLd
+    )
 endif()
 
 
@@ -92,28 +108,58 @@ else()
 endif()
 
 if(glslang_FOUND AND NOT TARGET glslang::glslang)
-  add_library(glslang::glslang UNKNOWN IMPORTED)
-  set_target_properties(glslang::glslang PROPERTIES IMPORTED_LOCATION "${glslang_LIBRARY}" INTERFACE_INCLUDE_DIRECTORIES "${glslang_INCLUDE_DIRS}")
+    add_library(glslang::glslang UNKNOWN IMPORTED)
+    set_target_properties(glslang::glslang PROPERTIES IMPORTED_LOCATION "${glslang_LIBRARY}" INTERFACE_INCLUDE_DIRECTORIES "${glslang_INCLUDE_DIRS}")
 
-  add_library(glslang::OSDependent UNKNOWN IMPORTED)
-  set_target_properties(glslang::OSDependent PROPERTIES IMPORTED_LOCATION "${OSDependent_LIBRARY}" INTERFACE_INCLUDE_DIRECTORIES "${glslang_INCLUDE_DIRS}")
+    add_library(glslang::OSDependent UNKNOWN IMPORTED)
+    set_target_properties(glslang::OSDependent PROPERTIES IMPORTED_LOCATION "${OSDependent_LIBRARY}" INTERFACE_INCLUDE_DIRECTORIES "${OSDependent_INCLUDE_DIRS}")
 
-  add_library(glslang::SPIRV UNKNOWN IMPORTED)
-  set_target_properties(glslang::SPIRV PROPERTIES IMPORTED_LOCATION "${SPIRV_LIBRARY}" INTERFACE_INCLUDE_DIRECTORIES "${spirv_INCLUDE_DIR}")
+    add_library(glslang::SPIRV UNKNOWN IMPORTED)
+    set_target_properties(glslang::SPIRV PROPERTIES IMPORTED_LOCATION "${SPIRV_LIBRARY}" INTERFACE_INCLUDE_DIRECTORIES "${spirv_INCLUDE_DIR}")
 
-  add_library(glslang::OGLCompiler UNKNOWN IMPORTED)
-  set_target_properties(glslang::OGLCompiler PROPERTIES IMPORTED_LOCATION "${OGLCompiler_LIBRARY}" INTERFACE_INCLUDE_DIRECTORIES "${glslang_INCLUDE_DIRS}")
+    if (SPIRV-Tools_LIBRARY)
+        add_library(glslang::SPIRV-Tools UNKNOWN IMPORTED)
+        set_target_properties(glslang::SPIRV-Tools PROPERTIES IMPORTED_LOCATION "${SPIRV-Tools_LIBRARY}" INTERFACE_INCLUDE_DIRECTORIES "${SPIRV-Tools__INCLUDE_DIR}")
+    endif()
 
-  add_library(glslang::HLSL UNKNOWN IMPORTED)
-  set_target_properties(glslang::HLSL PROPERTIES IMPORTED_LOCATION "${HLSL_LIBRARY}" INTERFACE_INCLUDE_DIRECTORIES "${glslang_INCLUDE_DIRS}")
+    if (SPIRV-Tools-opt_LIBRARY)
+        add_library(glslang::SPIRV-Tools-opt UNKNOWN IMPORTED)
+        set_target_properties(glslang::SPIRV-Tools-opt PROPERTIES IMPORTED_LOCATION "${SPIRV-Tools-opt_LIBRARY}" INTERFACE_INCLUDE_DIRECTORIES "${SPIRV-Tools-opt_INCLUDE_DIR}")
+    endif()
 
-  if(WIN32)
-    set_target_properties(glslang::glslang PROPERTIES IMPORTED_LOCATION_DEBUG "${glslang_LIBRARY_debug}")
-    set_target_properties(glslang::OSDependent PROPERTIES IMPORTED_LOCATION_DEBUG "${OSDependent_LIBRARY_debug}")
-    set_target_properties(glslang::SPIRV PROPERTIES IMPORTED_LOCATION_DEBUG "${SPIRV_LIBRARY_debug}")
-    set_target_properties(glslang::OGLCompiler PROPERTIES IMPORTED_LOCATION_DEBUG "${OGLCompiler_LIBRARY_debug}")
-    set_target_properties(glslang::HLSL PROPERTIES IMPORTED_LOCATION_DEBUG "${HLSL_LIBRARY_debug}")
-  endif()
+    add_library(glslang::OGLCompiler UNKNOWN IMPORTED)
+    set_target_properties(glslang::OGLCompiler PROPERTIES IMPORTED_LOCATION "${OGLCompiler_LIBRARY}" INTERFACE_INCLUDE_DIRECTORIES "${glslang_INCLUDE_DIRS}")
 
+    add_library(glslang::HLSL UNKNOWN IMPORTED)
+    set_target_properties(glslang::HLSL PROPERTIES IMPORTED_LOCATION "${HLSL_LIBRARY}" INTERFACE_INCLUDE_DIRECTORIES "${glslang_INCLUDE_DIRS}")
+
+    if(WIN32)
+        set_target_properties(glslang::glslang PROPERTIES IMPORTED_LOCATION_DEBUG "${glslang_LIBRARY_debug}")
+        set_target_properties(glslang::OSDependent PROPERTIES IMPORTED_LOCATION_DEBUG "${OSDependent_LIBRARY_debug}")
+        set_target_properties(glslang::SPIRV PROPERTIES IMPORTED_LOCATION_DEBUG "${SPIRV_LIBRARY_debug}")
+        if (SPIRV-Tools_LIBRARY_debug)
+            set_target_properties(glslang::SPIRV-Tools PROPERTIES IMPORTED_LOCATION_DEBUG "${SPIRV-Tools_LIBRARY_debug}")
+        endif()
+        if (SPIRV-Tools-opt_LIBRARY_debug)
+            set_target_properties(glslang::SPIRV-Tools-opt PROPERTIES IMPORTED_LOCATION_DEBUG "${SPIRV-Tools-opt_LIBRARY_debug}")
+        endif()
+        set_target_properties(glslang::OGLCompiler PROPERTIES IMPORTED_LOCATION_DEBUG "${OGLCompiler_LIBRARY_debug}")
+        set_target_properties(glslang::HLSL PROPERTIES IMPORTED_LOCATION_DEBUG "${HLSL_LIBRARY_debug}")
+    endif()
+
+    set(GLSLANG
+        glslang::glslang
+        glslang::OSDependent
+        glslang::SPIRV
+        glslang::OGLCompiler
+        glslang::HLSL
+    )
+    if (SPIRV-Tools_LIBRARY)
+        append(GLSLANG glslang::SPIRV-Tools)
+    endif()
+
+    if (SPIRV-Tools-opt_LIBRARY)
+        append(GLSLANG glslang::SPIRV-Tools-opt)
+    endif()
 endif()
 
