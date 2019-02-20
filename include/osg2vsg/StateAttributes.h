f0cfa0ab21dd1d6022f6b9d05bf9faabbb6d806b
Robert Osfield <robert@openscenegraph.com>
2019-02-20 17:21:28 +0000

    Added prelimary work on StateAttribute classes to use with vsg::StateGroup/StateSet.

---
+++ include/osg2vsg/StateAttributes.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <vsg/nodes/Node.h>
+
+#include <vsg/vk/GraphicsPipeline.h>
+#include <vsg/vk/PushConstants.h>
+#include <vsg/vk/CommandPool.h>
+#include <vsg/vk/DescriptorSet.h>
+#include <vsg/vk/DescriptorPool.h>
+#include <vsg/nodes/StateGroup.h>
+#include <vsg/traversals/CompileTraversal.h>
+
+namespace vsg
+{
+
+    class GraphicsPipelineAttribute : public Inherit<StateComponent, GraphicsPipelineAttribute>
+    {
+    public:
+        GraphicsPipelineAttribute(Allocator* allocator = nullptr);
+
+        void compile(Context& context) override;
+
+        void read(Input& input) override;
+        void write(Output& output) const override;
+
+
+        using Shaders = std::vector<ref_ptr<Shader>>;
+
+        // settings
+        // descriptorPool ..
+        uint32_t maxSets = 0;
+        DescriptorPoolSizes descriptorPoolSizes; // need to accumulate descriptorPoolSizes by looking at scene graph
+        // descriptorSetLayout ..
+        DescriptorSetLayoutBindings descriptorSetLayoutBindings;
+        PushConstantRanges pushConstantRanges;
+        VertexInputState::Bindings vertexBindingsDescriptions;
+        VertexInputState::Attributes vertexAttributeDescriptions;
+        Shaders shaders;
+        GraphicsPipelineStates pipelineStates;
+
+        // compiled objects
+        ref_ptr<BindPipeline> _bindPipeline;
+        ref_ptr<PushConstants> _projPushConstant;
+        ref_ptr<PushConstants> _viewPushConstant;
+    };
+    VSG_type_name(vsg::GraphicsPipelineAttribute)
+
+    class TextureAttribute : public Inherit<StateComponent, TextureAttribute>
+    {
+    public:
+        TextureAttribute(Allocator* allocator = nullptr);
+
+        void compile(Context& context);
+
+        // settings
+        uint32_t _bindingIndex = 0;
+        VkSamplerCreateInfo _samplerInfo;
+        ref_ptr<Data> _textureData;
+
+        // compiled objects
+        ref_ptr<vsg::BindDescriptorSets> _bindDescriptorSets;
+
+    };
+    VSG_type_name(vsg::TextureAttribute)
+
+}
---
+++ src/osg2vsg/StateAttributes.cpp
@@ -0,0 +1,167 @@
+#include <vsg/vk/BufferData.h>
+#include <vsg/vk/Descriptor.h>
+#include <vsg/vk/DescriptorSet.h>
+#include <vsg/vk/DescriptorPool.h>
+#include <vsg/vk/BindVertexBuffers.h>
+#include <vsg/vk/BindIndexBuffer.h>
+#include <vsg/vk/Draw.h>
+
+#include <vsg/io/ReaderWriter.h>
+
+#include <vsg/traversals/DispatchTraversal.h>
+
+#include <iostream>
+
+#include <osg2vsg/StateAttributes.h>
+
+using namespace vsg;
+
+#if 0
+#define DEBUG_OUTPUT std::cout
+#else
+#define DEBUG_OUTPUT if (false) std::cout
+#endif
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+// GraphicsPipelineAttribute
+//
+GraphicsPipelineAttribute::GraphicsPipelineAttribute(Allocator* allocator) :
+    Inherit(allocator)
+{
+}
+
+void GraphicsPipelineAttribute::read(Input& input)
+{
+    StateComponent::read(input);
+
+    shaders.resize(input.readValue<uint32_t>("NumShader"));
+    for (auto& shader : shaders)
+    {
+        shader = input.readObject<Shader>("Shader");
+    }
+}
+
+void GraphicsPipelineAttribute::write(Output& output) const
+{
+    StateComponent::write(output);
+
+    output.writeValue<uint32_t>("NumShader", shaders.size());
+    for (auto& shader : shaders)
+    {
+        output.writeObject("Shader", shader.get());
+    }
+}
+
+void GraphicsPipelineAttribute::compile(Context& context)
+{
+    DEBUG_OUTPUT<<"\nGraphicsPipelineAttribute::compile(Context& context) "<<this<<std::endl;
+
+    //
+    // set up descriptor layout and descriptor set and pipeline layout for uniforms
+    //
+    context.descriptorPool = DescriptorPool::create(context.device, maxSets, descriptorPoolSizes);
+    DEBUG_OUTPUT<<"  context.descriptorPool = "<<context.descriptorPool.get()<<std::endl;
+
+    context.descriptorSetLayout = DescriptorSetLayout::create(context.device, descriptorSetLayoutBindings);
+    DEBUG_OUTPUT<<"  context.descriptorSetLayout = "<<context.descriptorSetLayout.get()<<std::endl;
+
+    context.pipelineLayout = PipelineLayout::create(context.device, {context.descriptorSetLayout}, pushConstantRanges);
+    DEBUG_OUTPUT<<"  context.pipelineLayout = "<<context.pipelineLayout.get()<<std::endl;
+
+
+    ShaderModules shaderModules;
+    shaderModules.reserve(shaders.size());
+    for(auto& shader : shaders)
+    {
+        shaderModules.emplace_back(ShaderModule::create(context.device, shader));
+    }
+
+    auto shaderStages = ShaderStages::create(shaderModules);
+
+    DEBUG_OUTPUT<<"  shaderStages = "<<shaderStages.get()<<std::endl;
+
+    GraphicsPipelineStates full_pipelineStates = pipelineStates;
+    full_pipelineStates.emplace_back(context.viewport);
+    full_pipelineStates.emplace_back(shaderStages);
+    full_pipelineStates.emplace_back(VertexInputState::create(vertexBindingsDescriptions, vertexAttributeDescriptions));
+
+
+    ref_ptr<GraphicsPipeline> pipeline = GraphicsPipeline::create(context.device, context.renderPass, context.pipelineLayout, full_pipelineStates);
+
+    DEBUG_OUTPUT<<"  pipeline = "<<pipeline.get()<<std::endl;
+
+    _bindPipeline = BindPipeline::create(pipeline);
+
+    DEBUG_OUTPUT<<"  _bindPipeline = "<<_bindPipeline.get()<<std::endl;
+
+    if (context.projMatrix) _projPushConstant = PushConstants::create(VK_SHADER_STAGE_VERTEX_BIT, 0, context.projMatrix);
+    if (context.viewMatrix) _viewPushConstant = PushConstants::create(VK_SHADER_STAGE_VERTEX_BIT, 64, context.viewMatrix);
+    DEBUG_OUTPUT<<"GraphicsPipelineAttribute::compile(Context& context) finished "<<this<<"\n"<<std::endl;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+//
+// TextureAttribut
+//
+TextureAttribute::TextureAttribute(Allocator* allocator) :
+    Inherit(allocator)
+{
+    // set default sampler info
+    _samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+    _samplerInfo.minFilter = VK_FILTER_LINEAR;
+    _samplerInfo.magFilter = VK_FILTER_LINEAR;
+    _samplerInfo.addressModeU = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    _samplerInfo.addressModeV = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+    _samplerInfo.addressModeW = VK_SAMPLER_ADDRESS_MODE_REPEAT;
+#if 1
+    // requres Logical device to have deviceFeatures.samplerAnisotropy = VK_TRUE; set when creating the vsg::Device
+    _samplerInfo.anisotropyEnable = VK_TRUE;
+    _samplerInfo.maxAnisotropy = 16;
+#else
+    _samplerInfo.anisotropyEnable = VK_FALSE;
+    _samplerInfo.maxAnisotropy = 1;
+#endif
+    _samplerInfo.borderColor = VK_BORDER_COLOR_INT_OPAQUE_BLACK;
+    _samplerInfo.unnormalizedCoordinates = VK_FALSE;
+    _samplerInfo.compareEnable = VK_FALSE;
+    _samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
+}
+
+void TextureAttribute::compile(Context& context)
+{
+    ref_ptr<Sampler> sampler = Sampler::create(context.device, _samplerInfo, nullptr);
+    vsg::ImageData imageData = vsg::transferImageData(context.device, context.commandPool, context.graphicsQueue, _textureData, sampler);
+    if (!imageData.valid())
+    {
+        DEBUG_OUTPUT<<"Texture not created"<<std::endl;
+        return;
+    }
+
+    // set up DescriptorSet
+    vsg::ref_ptr<vsg::DescriptorSet> descriptorSet = vsg::DescriptorSet::create(context.device, context.descriptorPool, context.descriptorSetLayout,
+    {
+        vsg::DescriptorImage::create(_bindingIndex, 0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, vsg::ImageDataList{imageData})
+    });
+
+    if (descriptorSet)
+    {
+        // setup binding of descriptors
+        _bindDescriptorSets = vsg::BindDescriptorSets::create(VK_PIPELINE_BIND_POINT_GRAPHICS, context.pipelineLayout, vsg::DescriptorSets{descriptorSet}); // device dependent
+
+        DEBUG_OUTPUT<<"TextureAttribute::compile(() succeeded."<<std::endl;
+        DEBUG_OUTPUT<<"   imageData._sampler = "<<imageData._sampler.get()<<std::endl;
+        DEBUG_OUTPUT<<"   imageData._imageView = "<<imageData._imageView.get()<<std::endl;
+        DEBUG_OUTPUT<<"   imageData._imageLayout = "<<imageData._imageLayout<<std::endl;
+    }
+    else
+    {
+        DEBUG_OUTPUT<<"TextureAttribute::compile(() failed, descriptorSet not created."<<std::endl;
+        DEBUG_OUTPUT<<"   imageData._sampler = "<<imageData._sampler.get()<<std::endl;
+        DEBUG_OUTPUT<<"   imageData._imageView = "<<imageData._imageView.get()<<std::endl;
+        DEBUG_OUTPUT<<"   imageData._imageLayout = "<<imageData._imageLayout<<std::endl;
+        DEBUG_OUTPUT<<"   _textureData = "<<_textureData->width()<<", "<<_textureData->height()<<", "<<_textureData->depth()<<", "<<std::endl;
+    }
+}
+
