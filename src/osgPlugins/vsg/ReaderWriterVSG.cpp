2c86509ab3ee007e5048cdfaa5ee81cfdf3f3df5
Robert Osfield <robert@openscenegraph.com>
2019-03-13 17:43:33 +0000

    Added support for writing to vsga and vsgb fils using OSG ReaderWriter enabling one to write files using osgconv :
      osgconv -e vsg lz.osgt lz.vsga
    
    Or writeing out from a OSG application that has the VSG plugn on it's path.
    
      osgDB::writeNodeFile(*node, "myfile.vsga");

--- src/osgPlugins/vsg/ReaderWriterVSG.cpp
+++ src/osgPlugins/vsg/ReaderWriterVSG.cpp
@@ -7,6 +7,9 @@
 #include <osgDB/FileUtils>
 #include <osgDB/Registry>
 
+#include <osgUtil/Optimizer>
+#include <osgUtil/MeshOptimizers>
+
 #include <vsg/io/AsciiInput.h>
 #include <vsg/io/AsciiOutput.h>
 #include <vsg/io/BinaryInput.h>
@@ -16,6 +19,9 @@
 #include <vsg/nodes/Group.h>
 
 #include <osg2vsg/ImageUtils.h>
+#include <osg2vsg/ShaderUtils.h>
+#include <osg2vsg/GeometryUtils.h>
+#include <osg2vsg/SceneAnalysisVisitor.h>
 
 
 class ReaderWriterVSG : public osgDB::ReaderWriter
@@ -120,13 +126,65 @@ class ReaderWriterVSG : public osgDB::ReaderWriter
 
         virtual WriteResult writeNode(const osg::Node& node, const std::string& filename, const osgDB::ReaderWriter::Options* options) const
         {
+            osg::Node& osg_scene = const_cast<osg::Node&>(node);
+
             std::string ext = osgDB::getFileExtension(filename);
             if (!acceptsExtension(ext)) return WriteResult::FILE_NOT_HANDLED;
 
-            vsg::ref_ptr<vsg::Group> group = vsg::Group::create();
+            bool optimize = true;
+            bool writeToFileProgramAndDataSetSets = false;
+            vsg::Paths searchPaths = vsg::getEnvPaths("VSG_FILE_PATH");
 
-            if (group) return write(group, filename, options);
-            else return WriteResult::FILE_NOT_HANDLED;
+            if (optimize)
+            {
+                osgUtil::IndexMeshVisitor imv;
+                #if OSG_MIN_VERSION_REQUIRED(3,6,4)
+                imv.setGenerateNewIndicesOnAllGeometries(true);
+                #endif
+                osg_scene.accept(imv);
+                imv.makeMesh();
+
+                osgUtil::VertexCacheVisitor vcv;
+                osg_scene.accept(vcv);
+                vcv.optimizeVertices();
+
+                osgUtil::VertexAccessOrderVisitor vaov;
+                osg_scene.accept(vaov);
+                vaov.optimizeOrder();
+
+                osgUtil::Optimizer optimizer;
+                optimizer.optimize(&osg_scene, osgUtil::Optimizer::DEFAULT_OPTIMIZATIONS);
+            }
+
+
+            // Collect stats about the loaded scene
+            osg2vsg::SceneAnalysisVisitor sceneAnalysis;
+            sceneAnalysis.writeToFileProgramAndDataSetSets = writeToFileProgramAndDataSetSets;
+            osg_scene.accept(sceneAnalysis);
+
+            // build VSG scene
+            vsg::ref_ptr<vsg::Node> converted_vsg_scene = sceneAnalysis.createVSG(searchPaths);
+
+            if (converted_vsg_scene)
+            {
+                if (ext=="vsga")
+                {
+                    std::ofstream fout(filename);
+                    vsg::AsciiOutput output(fout);
+                    output.writeObject("Root", converted_vsg_scene);
+                    return WriteResult::FILE_SAVED;
+                }
+                else if (ext=="vsgb")
+                {
+                    std::ofstream fout(filename, std::ios::out | std::ios::binary);
+                    vsg::BinaryOutput output(fout);
+                    output.writeObject("Root", converted_vsg_scene);
+                    return WriteResult::FILE_SAVED;
+                }
+            }
+
+            return WriteResult::FILE_NOT_HANDLED;
+
         }
 
 };
