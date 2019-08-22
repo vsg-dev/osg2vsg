2a6d108a010f3cec63c2a7fe384499e44a5ffb5f
tomhog <thomas.hogarth@gmail.com>
2019-08-22 04:42:21 +0100

    interop example now working

--- applications/vsgglinterop/vsgglinterop.cpp
+++ applications/vsgglinterop/vsgglinterop.cpp
@@ -108,10 +108,13 @@ public:
         _handle(handle),
         _tiling(tiling),
         //_memory(0),
-        _isDedicated(isDedicated),
-        _srcLayout(GL_LAYOUT_GENERAL_EXT),
-        _dstLayout(GL_LAYOUT_GENERAL_EXT)
-    {}
+        _isDedicated(isDedicated)
+    {
+        setFilter(osg::Texture2D::MIN_FILTER, osg::Texture2D::LINEAR);
+        setFilter(osg::Texture2D::MAG_FILTER, osg::Texture2D::LINEAR);
+        setUseHardwareMipMapGeneration(false);
+        setResizeNonPowerOfTwoHint(false);
+    }
 
     void apply(osg::State& state) const
     {
@@ -122,7 +125,7 @@ public:
 
         if(textureObject)
         {
-
+            textureObject->bind(state);
         }
         else if(_image.valid() && _image->data())
         {
@@ -173,16 +176,21 @@ public:
 #endif
 
                 GLuint glTiling = _tiling == VK_IMAGE_TILING_LINEAR ? GL_LINEAR_TILING_EXT : GL_OPTIMAL_TILING_EXT;
+                
+                glPixelStorei(GL_UNPACK_ALIGNMENT, image->getPacking());
 
                 // create texture storage using the memory
                 _memExt->glTextureParameteri(textureObject->id(), GL_TEXTURE_TILING_EXT, glTiling);
-                _memExt->glTextureStorageMem2DEXT(textureObject->id(), 1, GL_RGBA8, _image->s(), _image->t(), memory, 0);
+                _memExt->glTextureStorageMem2DEXT(textureObject->id(), 1, texStorageSizedInternalFormat, _image->s(), _image->t(), memory, 0);
                 
-                glTexImage2D(GL_TEXTURE_2D, 0, _internalFormat,
-                    _textureWidth, _textureHeight, 0,
-                    (GLenum)_image->getPixelFormat(),
-                    (GLenum)_image->getDataType(),
-                    _image->data());
+                //extensions->glTexStorage2D(GL_TEXTURE_2D, _numMipmapLevels, texStorageSizedInternalFormat, _textureWidth, _textureWidth);
+
+                unsigned char* dataPtr = (unsigned char*)image->data();
+                glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0,
+                    _textureWidth, _textureHeight,
+                    (GLenum)image->getPixelFormat(),
+                    (GLenum)image->getDataType(),
+                    dataPtr);
 
                 textureObject->setAllocated(true);
             }
@@ -198,9 +206,6 @@ public:
     VkImageTiling _tiling;
     //GLuint _memory;
     bool _isDedicated;
-
-    GLenum _srcLayout;
-    GLenum _dstLayout;
 };
 
 std::set<VkImageTiling> getSupportedTiling()
@@ -258,6 +263,31 @@ namespace vsg
         {
         }
 
+        class MoveToTargetLayoutImageDataCommand : public Command
+        {
+        public:
+            MoveToTargetLayoutImageDataCommand(ImageData imageData) :
+                _imageData(imageData) {}
+
+            void dispatch(CommandBuffer& commandBuffer) const override
+            {
+                ref_ptr<Image> textureImage(_imageData._imageView->getImage());
+                VkImageLayout targetImageLayout = _imageData._imageLayout;
+
+                ImageMemoryBarrier postCopyImageMemoryBarrier(
+                    0, VK_ACCESS_SHADER_READ_BIT,
+                    VK_IMAGE_LAYOUT_UNDEFINED, targetImageLayout,
+                    textureImage);
+
+                postCopyImageMemoryBarrier.cmdPiplineBarrier(commandBuffer, VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT);
+            }
+
+            ImageData _imageData;
+
+        protected:
+            virtual ~MoveToTargetLayoutImageDataCommand(){}
+        };
+
         vsg::ImageData createSharedMemoryImageData(Context& context, Sampler* sampler, VkImageLayout targetImageLayout)
         {
             Device* device = context.device;
@@ -269,6 +299,7 @@ namespace vsg
                 _tiling = VK_IMAGE_TILING_OPTIMAL;
             }
 
+            // query chain
             VkPhysicalDeviceImageFormatInfo2 imageFormatInfo;
             imageFormatInfo.sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_IMAGE_FORMAT_INFO_2;
             imageFormatInfo.format = VK_FORMAT_R8G8B8A8_UNORM;
@@ -283,22 +314,24 @@ namespace vsg
 
             imageFormatInfo.pNext = &externalImageFormatInfo;
 
+            // results chain
             VkImageFormatProperties2 imageFormatProperties2;
             imageFormatProperties2.sType = VK_STRUCTURE_TYPE_IMAGE_FORMAT_PROPERTIES_2;
-            imageFormatProperties2.pNext = nullptr;
+
+            VkExternalImageFormatProperties externalImageFormatProperties;
+            externalImageFormatProperties.sType = VK_STRUCTURE_TYPE_EXTERNAL_IMAGE_FORMAT_PROPERTIES;
+            externalImageFormatProperties.pNext = nullptr;
+
+            imageFormatProperties2.pNext = &externalImageFormatProperties;
             
             vkGetPhysicalDeviceImageFormatProperties2(device->getPhysicalDevice()->getPhysicalDevice(), &imageFormatInfo, &imageFormatProperties2);
 
             VkImageFormatProperties imageFormatProperties = imageFormatProperties2.imageFormatProperties;
 
-            /*VkExternalImageFormatProperties externalImageFormatProperties;
-            externalImageFormatProperties = *(VkExternalImageFormatProperties*)imageFormatProperties2.pNext;
-
             if (externalImageFormatProperties.externalMemoryProperties.externalMemoryFeatures & VK_EXTERNAL_MEMORY_FEATURE_DEDICATED_ONLY_BIT)
             {
                 _dedicated = true;
-            }*/
-            _dedicated = false;
+            }
             
             VkImageCreateInfo imageCreateInfo = {};
             imageCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
@@ -322,18 +355,21 @@ namespace vsg
                 return ImageData();
             }            
 
+
             // Always add the export info to the memory allocation chain
             VkExportMemoryAllocateInfo exportAllocInfo;
             exportAllocInfo.sType = VK_STRUCTURE_TYPE_EXPORT_MEMORY_ALLOCATE_INFO;
             exportAllocInfo.handleTypes = memoryHandleType;
             exportAllocInfo.pNext = nullptr;
 
+            VkMemoryDedicatedAllocateInfo dedicatedMemAllocInfo;
             if (_dedicated)
             {
                 // Potentially add the dedicated memory allocation
-                VkMemoryDedicatedAllocateInfo dedicatedMemAllocInfo;
                 dedicatedMemAllocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_DEDICATED_ALLOCATE_INFO;
                 dedicatedMemAllocInfo.image = textureImage->image();
+                dedicatedMemAllocInfo.buffer = VK_NULL_HANDLE;
+                dedicatedMemAllocInfo.pNext = nullptr;
                 exportAllocInfo.pNext = &dedicatedMemAllocInfo;
             }
 
@@ -361,8 +397,8 @@ namespace vsg
             getHandleInfo.memory = *deviceMemory;
             getHandleInfo.pNext = nullptr;
 
-            auto vkGetMemoryWin32HandleKHR = PFN_vkGetMemoryWin32HandleKHR(vkGetDeviceProcAddr(device->getDevice(), "vkGetMemoryWin32HandleKHR"));
-            vkGetMemoryWin32HandleKHR(device->getDevice(), &getHandleInfo, &_memoryHandle);
+            auto vkGetMemoryWin32HandleKHR2 = PFN_vkGetMemoryWin32HandleKHR(vkGetDeviceProcAddr(device->getDevice(), "vkGetMemoryWin32HandleKHR"));
+            vkGetMemoryWin32HandleKHR2(device->getDevice(), &getHandleInfo, &_memoryHandle);
 
             VkImageViewCreateInfo createInfo = {};
             createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
@@ -381,7 +417,7 @@ namespace vsg
 
             ImageData imageData(sampler, textureImageView, targetImageLayout);
 
-            //context.copyImageDataCommands.emplace_back(new CopyAndReleaseImageDataCommand(stagingBufferData, imageData, mipLevels));
+            context.commands.emplace_back(new MoveToTargetLayoutImageDataCommand(imageData));
 
             return imageData;
         }
@@ -621,27 +657,95 @@ vsg::ref_ptr<vsg::Viewer> createVsgViewer(osg::ref_ptr<osg::Image> image)
     return viewer;
 }
 
+void buildOsgDebugScene(osgViewer::Viewer* viewer, osg::Texture2D* texture)
+{
+    osg::Geometry* quadgeom = osg::createTexturedQuadGeometry(osg::Vec3(0.0f, 0.0f, 0.0f), osg::Vec3(800.0f, 0.0f, 0.0f), osg::Vec3(0.0f, 0.0f, 600.0f));
+    quadgeom->setUseVertexBufferObjects(true);
+    quadgeom->setUseDisplayList(false);
+    osg::Geode* quadgeode = new osg::Geode();
+    quadgeode->addDrawable(quadgeom);
+
+    osg::StateSet* stateset = quadgeode->getOrCreateStateSet();
+
+    osg::Program* program = new osg::Program();
+
+    const char* QuadShaderVert =
+        "#version 450\n"
+        "in vec4 osg_Vertex;\n"
+        "in vec4 osg_MultiTexCoord0;\n"
+        "out highp vec2 v_texcoord;\n"
+        "uniform mat4 osg_ModelViewProjectionMatrix;\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = osg_ModelViewProjectionMatrix * osg_Vertex;\n"
+        "    v_texcoord = osg_MultiTexCoord0.xy;\n"
+        "}\n";
+
+    const char* QuadShaderFrag =
+        "#version 450\n"
+        "in highp vec2 v_texcoord;\n"
+        "uniform sampler2D texture0;\n"
+        "out lowp vec4 fragColor;\n"
+        "void main()\n"
+        "{\n"
+        "    fragColor = texture(texture0, v_texcoord.xy);\n"
+        "}\n";
+
+    program->addShader(new osg::Shader(osg::Shader::VERTEX, QuadShaderVert));
+    program->addShader(new osg::Shader(osg::Shader::FRAGMENT, QuadShaderFrag));
+    stateset->setAttributeAndModes(program, osg::StateAttribute::ON);
+
+    stateset->setTextureAttribute(0, texture);
+    stateset->addUniform(new osg::Uniform("texture0", (unsigned int)0));
+
+    viewer->setSceneData(quadgeode);
+}
+
 int main(int /*argc*/, char** /*argv*/)
 {
+    osg::setNotifyLevel(osg::WARN);
+    osg::ref_ptr<osg::Image> image = osgDB::readRefImageFile("./image.png");
+
+    osg::ref_ptr< osg::GraphicsContext::Traits > traits = new osg::GraphicsContext::Traits();
+    traits->x = 20; traits->y = 20;
+    traits->width = 800; traits->height = 600;
+    traits->windowDecoration = true;
+    traits->doubleBuffer = true;
+    traits->glContextVersion = "4.5";
+    osg::ref_ptr< osg::GraphicsContext > gc = osg::GraphicsContext::createGraphicsContext(traits.get());
+    if (!gc.valid())
+    {
+        osg::notify(osg::FATAL) << "Unable to create OpenGL v" << "4.5" << " context." << std::endl;
+        return(1);
+    }
+
     osg::ref_ptr<osgViewer::Viewer> osgviewer = new osgViewer::Viewer();
     osgviewer->setThreadingModel(osgViewer::ViewerBase::SingleThreaded);
-
-    osgviewer->realize();
-    osgviewer->getCamera()->getGraphicsContext()->makeCurrent();
+    osgviewer->setCameraManipulator(nullptr);
+    osgviewer->getCamera()->setViewport(new osg::Viewport(0.0f, 0.0f, 800.0f, 600.0f));
+    osgviewer->getCamera()->setViewMatrix(osg::Matrix::identity());
+    osgviewer->getCamera()->setProjectionMatrixAsOrtho(0, 800.0f, 0, 600.0f, 0.0, 100);
+    osgviewer->getCamera()->setGraphicsContext(gc);
+    gc->realize();
+    
+    gc->makeCurrent();
 
     if (!_memExt.valid()) _memExt = new GLMemoryObjectExtensions(0);
-
-    osg::ref_ptr<osg::Image> image = osgDB::readRefImageFile("./image.png");
 
     vsg::ref_ptr<vsg::Viewer> vsgviewer = createVsgViewer(image);
 
     _sharedTexture = new SharedTexture2D(_sharedImage->_memoryHandle, _sharedImage->_tiling, _sharedImage->_dedicated, image.get());
+    //_sharedTexture = new SharedTexture2D(0, VkImageTiling::VK_IMAGE_TILING_OPTIMAL, false, image.get());
     _sharedTexture->apply(*osgviewer->getCamera()->getGraphicsContext()->getState());
 
+    gc->releaseContext();
 
-    osgviewer->getCamera()->getGraphicsContext()->releaseContext();
+    //buildOsgDebugScene(osgviewer, _sharedTexture.get());
+    //buildOsgDebugScene(osgviewer, new osg::Texture2D(image.get()));
+    //osgviewer->run();
 
     // main frame loop
+    
     while (vsgviewer->advanceToNextFrame())
     {
         // pass any events into EventHandlers assigned to the Viewer
@@ -651,6 +755,6 @@ int main(int /*argc*/, char** /*argv*/)
 
         vsgviewer->submitNextFrame();
     }
-
+    
     return 0;
 }
