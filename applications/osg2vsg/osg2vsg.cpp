74de87e6004ca1e85589aa027e487808b6607d63
Robert Osfield <robert@openscenegraph.com>
2019-10-25 17:46:08 +0100

    Changed to use of mat4 to ensure it's compatible with the new GLSL constructor convertion

--- applications/osg2vsg/osg2vsg.cpp
+++ applications/osg2vsg/osg2vsg.cpp
@@ -80,10 +80,10 @@ namespace vsg
 
             _path->getMatrix(time, matrix);
 
-            vsg::dmat4 vsg_matrix(matrix(0,0), matrix(1,0), matrix(2,0), matrix(3,0),
-                                  matrix(0,1), matrix(1,1), matrix(2,1), matrix(3,1),
-                                  matrix(0,2), matrix(1,2), matrix(2,2), matrix(3,2),
-                                  matrix(0,3), matrix(1,3), matrix(2,3), matrix(3,3));
+            vsg::dmat4 vsg_matrix(matrix(0, 0), matrix(0, 1), matrix(0, 2), matrix(0, 3),
+                                  matrix(1, 0), matrix(1, 1), matrix(1, 2), matrix(1, 3),
+                                  matrix(2, 0), matrix(2, 1), matrix(2, 2), matrix(2, 3),
+                                  matrix(3, 0), matrix(3, 1), matrix(3, 2), matrix(3, 3));
 
             _lookAt->set(vsg_matrix);
 
